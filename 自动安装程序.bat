6dca844c063c70e3b7d59d9f33c108b4a02246d2
Morganwan90 <61633409+Morganwan90@users.noreply.github.com>
2023-03-23 16:18:18 +0800

    Add files via upload

--- 自动安装程序.bat
+++ 自动安装程序.bat
@@ -1,19 +1,31 @@
 @echo off
-echo 尝试匹配模板地址...
 echo =========================================================================
 echo =========================================================================
+echo 本程序将采取直接替换默认模板方式安装，如用户曾【自定义标题格式】、【安装过其他类似插件】等将会被重置
+choice /C YN /N /M "是否继续（按下Y）或退出程序（按下N）？"
+if errorlevel 2 exit
+echo 继续执行程序...
+echo =========================================================================
+echo 准备备份旧模板...
+echo ===============如出现选择文件或目录，请选择文件（按F）======================
 if exist "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates" ( 
-xcopy "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal.dotm" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal-old.dotm"/y/f || goto error1
-xcopy 招股书排版*.dotm "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal.dotm"/y/f && (goto succeed1) || goto error2
+xcopy "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal.dotm" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal-old.*" /y/f || goto error1
+echo =========旧模板备份成功============
+echo 准备安装模板...
+xcopy 招股书排版*.dotm "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal.dotm" /y/f && (goto succeed1) || goto error2
 ) else ( 
 echo 默认模板地址匹配失败，尝试匹配PA标装机模板地址...
+echo 准备备份旧模板...
+echo ===============如出现选择文件或目录，请选择文件（按F）======================
 if exist "D:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates" ( 
-xcopy "D:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal.dotm" "D:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal-old.dotm"/y/f || goto error1
+xcopy "D:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal.dotm" "D:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal-old.*"/y/f || goto error1
+echo =========旧模板备份成功============
+echo 准备安装模板...
 xcopy 招股书排版*.dotm "D:\Users\%USERNAME%\AppData\Roaming\Microsoft\Templates\Normal.dotm" /y/f && (goto succeed2) || goto error2
 ) else ( 
 echo =========================================================================
 echo =========================================================================
-echo 【地址提取失败】未成功提取地址，请手动替换
+echo 【地址提取失败】未成功提取地址，请参照README文件手动替换
 ) 
 ) 
 
